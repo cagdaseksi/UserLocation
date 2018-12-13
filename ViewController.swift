4ff1725dab766281dee4e3135c23ce66e95d9b4b
Çağdaş Ekşi <cagdas.eksi@hotmail.com>
2018-12-13 23:52:05 +0300

    First commit
    Get user location.

---
+++ ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  UserLocation
+//
+//  Created by cagdas on 13.12.2018.
+//  Copyright © 2018 cagdas. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    //Map
+    @IBOutlet weak var map: MKMapView!
+    
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location = locations[0]
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        map.setRegion(region, animated: true)
+        
+        print(location.altitude)
+        print(location.speed)
+        
+        self.map.showsUserLocation = true
+    }
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+}
