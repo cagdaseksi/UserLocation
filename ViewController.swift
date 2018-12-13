d1ab333bd2fa5c1648f977c0dcbc8ea94096ebb0
Çağdaş Ekşi <cagdas.eksi@hotmail.com>
2018-12-14 00:52:58 +0300

    update.

--- ViewController.swift
+++ ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  UserLocation
 //
-//  Created by cagdas on 13.12.2018.
+//  Created by cagdas on 14.12.2018.
 //  Copyright © 2018 cagdas. All rights reserved.
 //
 
@@ -12,40 +12,38 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
-    //Map
     @IBOutlet weak var map: MKMapView!
     
     let manager = CLLocationManager()
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
-    {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
         let location = locations[0]
         
         let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
         let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
         let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
-        map.setRegion(region, animated: true)
+        self.map.setRegion(region, animated: true)
         
         print(location.altitude)
         print(location.speed)
+        print(" latitude: \(location.coordinate.latitude), longitude: \(location.coordinate.longitude)")
         
         self.map.showsUserLocation = true
+        
     }
     
-    
-    override func viewDidLoad()
-    {
+    override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
         
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
+        
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
     
 }
+
+
